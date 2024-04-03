f0dc09b8da8decfb8ee413d43b94b1e57856ac2c
M.Talha <talha424588@gmail.com>
2024-04-03 14:47:15 +0500

    register user complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,47 @@
-function Home()
-{
-    return (
-        <>
-        <h3>Home page</h3>
-        </>
-    )
+import React from "react";
+import { Card, Container, } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+
+function Home() {
+  return (
+    <>
+      <Container className="mt-2">
+      <Card>
+        <Card.Body>
+          <Table bordered className="m-0">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First</th>
+                <th>Last</th>
+                <th>Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
+    </>
+  );
 }
 export default Home;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,9 +1,57 @@
-function Register()
-{
+import { useState } from "react";
+import { Card, Container, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom"; // Import useHistory hook
+
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate(); // Initialize useHistory hook
+
+    async function register(e) {
+        let userData = { name, email, password };
+        e.preventDefault();
+        let result = await fetch(`http://127.0.0.1:3001/api/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(userData)
+        });
+        result = await result.json();
+        localStorage.setItem("user-info", JSON.stringify(result));
+        navigate('/');
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div className="my-4">
+            <Container className="col-lg-4">
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Register</Card.Title>
+                        <Form>
+                            <Form.Group controlId="formBasicName">
+                                <Form.Label className="">Name</Form.Label>
+                                <Form.Control value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter name" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label className="">Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label className="text-left">Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" className="mt-3" onClick={register}>
+                                Register
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
         </div>
     );
 }
+
 export default Register;
